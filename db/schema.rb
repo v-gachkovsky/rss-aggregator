ead1443af0fb9211f692286fe9e79cd27684f18d
Evsign <evsign@live.ru>
2014-07-27 02:41:19 +0300

    Added and migrate Feed model, updated Group for has_many

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140726230507) do
+ActiveRecord::Schema.define(version: 20140726232255) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "feeds", force: true do |t|
+    t.text     "feed"
+    t.integer  "group_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "feeds", ["group_id"], name: "index_feeds_on_group_id", using: :btree
 
   create_table "groups", force: true do |t|
     t.string   "group"
