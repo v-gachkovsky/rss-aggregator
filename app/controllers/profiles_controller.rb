25594f6a973ab9d24f82573fcc5aafc271d13e0d
v.gachkovsky <v.gachkovsky@gmail.com>
2014-08-04 02:48:18 +0300

    Strong parameters apply

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,42 @@
+class ProfilesController < Devise::RegistrationsController
+  def update
+    @user = User.find(current_user.id)
+
+    successfully_updated = if needs_password?(@user, params)
+      @user.update_with_password(account_update_params)
+    else
+      params[:user].delete(:current_password)
+      @user.update_without_password(account_update_params)
+    end
+
+    if successfully_updated
+      set_flash_message :notice, :updated
+      # Sign in the user bypassing validation in case their password changed
+      sign_in @user, :bypass => true
+      redirect_to after_update_path_for(@user)
+    else
+      render "edit"
+    end
+  end
+
+  private
+
+  def needs_password?(user, params)
+    user.email != params[:user][:email] ||
+    params[:user][:password].present? ||
+    params[:user][:password_confirmation].present?
+  end
+
+ def sign_up_params
+   params.require(:user).permit(:email, :password, :password_confirmation)
+ end
+
+ def account_update_params
+   params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
+ end
+
+ def after_update_path_for(resource)
+   edit_user_registration_path
+ end
+
+end
