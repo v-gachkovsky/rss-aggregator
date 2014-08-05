f303858531bb49ff45466e5195c34e413eecf94a
v.gachkovsky <v.gachkovsky@gmail.com>
2014-08-05 16:23:22 +0300

    modify groups_controller and add specs

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,45 @@
 class GroupsController < ApplicationController
-	before_action :authenticate_user!
-	def index
-		@groups = Group.all.includes(:feeds)
-	end
+
+  before_action :find_group, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!
+
+  def index
+    @groups = Group.includes(:feeds)
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = current_user.groups.new(params.require(:group).permit(:name))
+    if group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @group.update(params.require(:group).permit(:name))
+      redirect_to groups_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @group.destroy
+    redirect_to projects_path
+  end
+
+  private
+
+  def find_group
+  	@group = Group.find(params[:id])
+  end
+
 end
