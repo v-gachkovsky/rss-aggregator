80592ebe9984e9f3d41c93a606945db966465ed0
v.gachkovsky <v.gachkovsky@gmail.com>
2014-08-06 12:29:12 +0300

    Fix all test and modify GroupsController

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,12 +1,11 @@
 class GroupsController < ApplicationController
 
   before_action :find_group, only: [:edit, :update, :destroy]
- # before_action :authenticate_user!
+  before_action :authenticate_user!
 
   def index
-    #@groups = current_user.groups.includes(:feeds)
-    @groups = Group.all
-   
+    @groups = current_user.groups.includes(:feeds)
+    #@groups = Group.all
   end
 
   def new
@@ -14,8 +13,9 @@ class GroupsController < ApplicationController
   end
 
   def create
-    #@group = current_user.groups.new(params.require(:group).permit(:name))
-    @group = Group.new
+    # @group = current_user.groups.new(params.require(:group).permit(:name))
+    # @group = Group.new
+    @group = Group.new(group_params)
     if @group.save
       redirect_to groups_path
     else
@@ -24,11 +24,10 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:id])
   end
 
   def update
-    if @group.update(params.require(:group).permit(:name))
+    if @group.update(group_params)
       redirect_to groups_path
     else
       render :edit
@@ -44,6 +43,10 @@ class GroupsController < ApplicationController
 
   def find_group
     @group = Group.find(params[:id])
+  end
+
+  def group_params
+    params.require(:group).permit(:name)
   end
 
 end
