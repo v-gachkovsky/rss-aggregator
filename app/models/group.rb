4546534a00c6af5c18ce6adcd2d7902f79ed33f7
Natallika <penkratnat@tut.by>
2014-08-06 23:33:09 +0300

    feed, group, tizer validation - ok, user - only validates_associated was added

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,7 @@
 class Group < ActiveRecord::Base
   belongs_to :user
   has_many :feeds
+
+  validates :name, length: { in: 2..100 }, uniqueness: { scope: :user, message: "should be unique" }, :presence => true
+  validates_associated :feeds
 end
--- app/models/tizer.rb
+++ app/models/tizer.rb
@@ -1,3 +1,6 @@
 class Tizer < ActiveRecord::Base
   belongs_to :feed
+
+  validates :name, length: { in: 2..50 }
+  
 end
