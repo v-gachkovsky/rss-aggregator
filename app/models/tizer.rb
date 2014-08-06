2f5049c881df85b6184369debaf244e52727dc8c
Natallika <penkratnat@tut.by>
2014-08-06 23:52:43 +0300

    user validations added, tizer validations was fixed

--- app/models/tizer.rb
+++ app/models/tizer.rb
@@ -1,6 +1,6 @@
 class Tizer < ActiveRecord::Base
   belongs_to :feed
 
-  validates :name, length: { in: 2..50 }
+  validates :name, length: { in: 2..200 }
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :groups
 
+  validates :email, length: { in: 2..100 }, uniqueness: true, :format => { with: /.+@.+\..+/i }, :presence => true
+  validates :encrypted_password, length: { in: 8..100 }, :presence => true
   validates_associated :groups
 end
